c49afc6f45ed9c412db11e6d0f9b8dcaa012ae08
admin <1518483751@qq>
2025-07-20 14:35:10 +0800

    项目初始化

---
+++ gitops-project/src/main/java/com/example/gitopsproject/GitopsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitopsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitopsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitopsProjectApplication.class, args);
+    }
+
+}
